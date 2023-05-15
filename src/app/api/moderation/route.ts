39fc1853907b94c808b84449cf030584ae2e4e33
Piotr Pupczyk <ppsirius@gmail.com>
2023-05-15 12:05:36 +0200

    update moderation

---
+++ src/app/api/moderation/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+
+export interface ModerationAPI {
+  data: ArrayElement[];
+}
+
+export interface ArrayElement {
+  results: Data[];
+}
+
+export interface Data {
+  flagged: boolean;
+  categories: Categories;
+  category_scores: CategoryScores;
+}
+
+export interface Categories {
+  sexual: boolean;
+  hate: boolean;
+  violence: boolean;
+  'self-harm': boolean;
+  'sexual/minors': boolean;
+  'hate/threatening': boolean;
+  'violence/graphic': boolean;
+}
+
+export interface CategoryScores {
+  sexual: number;
+  hate: number;
+  violence: number;
+  'self-harm': number;
+  'sexual/minors': number;
+  'hate/threatening': number;
+  'violence/graphic': number;
+}
+
+export async function POST(req: Request) {
+  const request = await req.json();
+
+  const requestBody: ModerationAPI = request[0];
+
+  const flagged = requestBody.data.map((data) => {
+    return +data.results[0].flagged;
+  });
+
+  return NextResponse.json({ data: flagged });
+}
